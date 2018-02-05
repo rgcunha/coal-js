8bfb04100f74f0a2d68e1d63027babe5d78f38ae
Ruben Cunha <ruben.cunha@gmail.com>
2018-02-05 02:17:58 +0100

    Refactor Client class to use Resources

---
+++ test/fixtures/index.js
@@ -0,0 +1,17 @@
+import tokens from './tokens';
+import version from './version';
+import myAccount from './my-account';
+import sites from './sites';
+import currentSite from './current-site';
+import contentTypes from './content-types';
+import contentTypeEntries from './content-type-entries';
+
+export {
+  tokens,
+  version,
+  myAccount,
+  sites,
+  currentSite,
+  contentTypes,
+  contentTypeEntries
+}
